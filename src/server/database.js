5509116ab2dd50aa9eb78642bf864e6c5e03f586
ramiro-luengo <ramirobmds@gmail.com>
2022-10-18 12:45:49 -0300

    add database connection retry

--- src/server/database.js
+++ src/server/database.js
@@ -2,20 +2,32 @@ const mongoose = require("mongoose");
 const getLogger = require("../utils/logger");
 const logger = getLogger(__filename);
 
+const CONNECTION_RETRY = 3;
+let retry = 0;
+
 const startDatabase = () => {
-  // Connect Database.
-  const uri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_SERVICE_URL}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
+  const uri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_SERVICE_URL}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority&authSource=admin`;
 
   mongoose
     .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => logger.info("Database connected successfully"))
-    .catch((err) =>
-      logger.info({
-        dbUrl: uri,
-        message: "Database failed to connect",
-        trace: err,
-      })
-    );
+    .catch((err) => {
+      if (retry <= CONNECTION_RETRY) {
+        retry++;
+        logger.info({
+          dbUrl: uri,
+          message: `Database failed to connect, retry count:${retry}`,
+          trace: err,
+        });
+        setTimeout(startDatabase, 1000);
+      } else {
+        logger.info({
+          dbUrl: uri,
+          message: `Retry count exceeded.`,
+          trace: err,
+        });
+      }
+    });
 };
 
 module.exports = startDatabase;
