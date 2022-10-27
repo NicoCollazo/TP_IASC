e59ac0aa15d94a7b56a6f9117888a37bbea5448b
ramiro-luengo <ramirobmds@gmail.com>
2022-10-27 11:48:47 -0300

    add socket to app

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 import DragList from "./Components/Draglist";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import { SocketContext, socket } from "./context/socket";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
-  return <DragList />;
+	return (
+		<SocketContext.Provider value={socket}>
+			<DragList />
+		</SocketContext.Provider>
+	);
 }
 
 export default App;
