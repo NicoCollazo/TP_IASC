7bc1c670dd517b2440b6e00ed1820418a9564c33
ramiro-luengo <ramirobmds@gmail.com>
2022-10-31 15:00:44 -0300

    extract socket url to const

--- front/src/context/socket.js
+++ front/src/context/socket.js
@@ -1,7 +1,9 @@
 import socketio from "socket.io-client";
 import React from "react";
 
-export const socket = socketio.connect("http://localhost:8081", {
+const SOCKET_URL = `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_SOCKET_PORT}`;
+
+export const socket = socketio(SOCKET_URL, {
 	withCredentials: true,
 });
 export const SocketContext = React.createContext();
