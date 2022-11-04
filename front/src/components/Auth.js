6f882220209b4e36e686f5fb66ee9ceb7c757f12
NicoCollazo <48805854+NicoCollazo@users.noreply.github.com>
2022-11-03 23:08:29 -0300

    Added login when pressing the Enter key on the password text field

--- front/src/components/Auth.js
+++ front/src/components/Auth.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 import * as React from "react";
 import {
-	Avatar,
-	Button,
-	CssBaseline,
-	TextField,
-	Box,
-	Typography,
-	Container,
-	createTheme,
-	ThemeProvider,
-	CardHeader,
-	IconButton,
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Box,
+  Typography,
+  Container,
+  createTheme,
+  ThemeProvider,
+  CardHeader,
+  IconButton,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
@@ -21,90 +21,97 @@ const AUTH_URL = `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_A
 const theme = createTheme();
 
 export default function Auth() {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
+  const login = (event) => {
+    if (event.key === "Enter") {
+      this.handleSubmit(event);
+    }
+  };
 
-		const data = new FormData(event.currentTarget);
-		const loginValues = {
-			username: data.get("username"),
-			password: data.get("password"),
-		};
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-		axios
-			.post(AUTH_URL, loginValues, { withCredentials: true })
-			.then((response) => {
-				if (response.data.token !== undefined) {
-					navigate("/workspace");
-				}
-			})
-			.catch(function (error) {
-				console.log(error);
-				// Display error message on screen.
-			});
-	};
+    const data = new FormData(event.currentTarget);
+    const loginValues = {
+      username: data.get("username"),
+      password: data.get("password"),
+    };
 
-	return (
-		<ThemeProvider theme={theme}>
-			<Container component="main" maxWidth="xs">
-				<Box sx={{ padding: 4 }}>
-					<Typography variant="h6" color="initial">
-						WELCOME TO OUR TODO LIST APP
-					</Typography>
-				</Box>
-				<CssBaseline />
-				<Box
-					sx={{
-						marginTop: 8,
-						display: "flex",
-						flexDirection: "column",
-						alignItems: "center",
-					}}
-				>
-					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-						<LockOutlinedIcon />
-					</Avatar>
-					<Typography component="h1" variant="h5">
-						Sign in
-					</Typography>
-					<Box
-						component="form"
-						onSubmit={handleSubmit}
-						noValidate
-						sx={{ mt: 1 }}
-					>
-						<TextField
-							margin="normal"
-							required
-							fullWidth
-							id="username"
-							label="Username"
-							name="username"
-							autoComplete="username"
-							autoFocus
-						/>
-						<TextField
-							margin="normal"
-							required
-							fullWidth
-							name="password"
-							label="Password"
-							type="password"
-							id="password"
-							autoComplete="current-password"
-						/>
-						<Button
-							type="submit"
-							fullWidth
-							variant="contained"
-							sx={{ mt: 3, mb: 2 }}
-						>
-							Sign In
-						</Button>
-					</Box>
-				</Box>
-			</Container>
-		</ThemeProvider>
-	);
+    axios
+      .post(AUTH_URL, loginValues, { withCredentials: true })
+      .then((response) => {
+        if (response.data.token !== undefined) {
+          navigate("/workspace");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+        // Display error message on screen.
+      });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <Box sx={{ padding: 4 }}>
+          <Typography variant="h6" color="initial">
+            WELCOME TO OUR TODO LIST APP
+          </Typography>
+        </Box>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onKeyUp="{this.login}"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
