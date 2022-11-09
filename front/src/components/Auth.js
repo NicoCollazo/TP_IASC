e6ac3564a90104bf634a4d99fcf31480c82e53bd
ramiro-luengo <ramirobmds@gmail.com>
2022-11-09 14:20:48 -0300

    reformat with prettier and use login event

--- front/src/components/Auth.js
+++ front/src/components/Auth.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 import { useState } from "react";
 import {
-  Avatar,
-  Button,
-  CssBaseline,
-  TextField,
-  Box,
-  Typography,
-  Container,
-  createTheme,
-  ThemeProvider,
-  Snackbar,
-  Alert,
+	Avatar,
+	Button,
+	CssBaseline,
+	TextField,
+	Box,
+	Typography,
+	Container,
+	createTheme,
+	ThemeProvider,
+	Snackbar,
+	Alert,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { LockOutlined as LockOutlinedIcon } from "@mui/icons-material";
@@ -21,113 +21,113 @@ const AUTH_URL = `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_A
 const theme = createTheme();
 
 export default function Auth() {
-  const [errorNotif, setErrorNotif] = useState({ open: false, message: "" });
-  const navigate = useNavigate();
+	const [errorNotif, setErrorNotif] = useState({ open: false, message: "" });
+	const navigate = useNavigate();
 
-  const login = (event) => {
-    if (event.key === "Enter") {
-      this.handleSubmit(event);
-    }
-  };
+	const login = (event) => {
+		if (event.key === "Enter") {
+			this.handleSubmit(event);
+		}
+	};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+	const handleSubmit = (event) => {
+		event.preventDefault();
 
-    const data = new FormData(event.currentTarget);
-    const loginValues = {
-      username: data.get("username"),
-      password: data.get("password"),
-    };
+		const data = new FormData(event.currentTarget);
+		const loginValues = {
+			username: data.get("username"),
+			password: data.get("password"),
+		};
 
-    axios
-      .post(AUTH_URL, loginValues, { withCredentials: true })
-      .then((response) => {
-        if (response.data.token !== undefined) {
-          navigate("/workspace");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        const errMsg = err.response.data.error;
-        setErrorNotif({ open: true, message: errMsg });
-      });
-  };
+		axios
+			.post(AUTH_URL, loginValues, { withCredentials: true })
+			.then((response) => {
+				if (response.data.token !== undefined) {
+					navigate("/workspace");
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				const errMsg = err.response.data.error;
+				setErrorNotif({ open: true, message: errMsg });
+			});
+	};
 
-  const onCloseNotif = () => {
-    setErrorNotif({ open: false, message: "" });
-  };
+	const onCloseNotif = () => {
+		setErrorNotif({ open: false, message: "" });
+	};
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <Box sx={{ padding: 4 }}>
-          <Typography variant="h6" color="initial">
-            WELCOME TO OUR TODO LIST APP
-          </Typography>
-        </Box>
-        <CssBaseline />
-        <Snackbar
-          open={errorNotif.open}
-          onClose={onCloseNotif}
-          autoHideDuration={3000}
-          anchorOrigin={{ vertical: "top", horizontal: "center" }}
-        >
-          <Alert onClose={onCloseNotif} severity="error">
-            {errorNotif.message}
-          </Alert>
-        </Snackbar>
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              autoComplete="username"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onKeyUp="{this.login}"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<Box sx={{ padding: 4 }}>
+					<Typography variant="h6" color="initial">
+						WELCOME TO OUR TODO LIST APP
+					</Typography>
+				</Box>
+				<CssBaseline />
+				<Snackbar
+					open={errorNotif.open}
+					onClose={onCloseNotif}
+					autoHideDuration={3000}
+					anchorOrigin={{ vertical: "top", horizontal: "center" }}
+				>
+					<Alert onClose={onCloseNotif} severity="error">
+						{errorNotif.message}
+					</Alert>
+				</Snackbar>
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Sign in
+					</Typography>
+					<Box
+						component="form"
+						onSubmit={handleSubmit}
+						noValidate
+						sx={{ mt: 1 }}
+					>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="username"
+							label="Username"
+							name="username"
+							autoComplete="username"
+							autoFocus
+						/>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Password"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+							onKeyUp={login}
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Sign In
+						</Button>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	);
 }
